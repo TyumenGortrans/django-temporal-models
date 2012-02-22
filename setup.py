e82ae91b3a3f5a12b392f9ddece5180f014a5c31
Andrey Zevakin <azevakin@gmail.com>
2012-02-22 16:04:33 +0600

    добавил setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import os
+from distutils.core import setup
+
+setup(
+    name='Django-temporal-models',
+    version='0.0.1',
+    description='Implementation of supporting temporality in Django models',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    url='https://github.com/TyumenGortrans/django-temporal-models',
+    author='TyumenGortrans',
+    author_email='django.temporal.models@gmail.com',
+    packages=['temporal', 'temporal.models'],
+    license='MIT License',
+    platforms='OS Independent',
+    classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: Russian',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Framework :: Django',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
